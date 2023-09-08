c2ebd4cd99c686b571296d668d8b5c874fc2037b
Ridho <ridhofullstuck@gmail.com>
2023-09-08 11:23:04 +0700

    commit pertama

---
+++ resources/views/main.blade.php
@@ -0,0 +1,331 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GoogleRedesign</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <section>
+            <header>
+                <a class="navbar-brand" href="#"><img src="img/logo.png" alt="" class="logo-google"></a>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2 rounded-pill frm" type="search" placeholder="tips agar cepat tidur"
+                        aria-label="Search">
+                    {{-- <button class="btn btn-outline-success" type="submit"><svg xmlns="http://www.w3.org/2000/svg"
+                            width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg></button> --}}
+                </form>
+                <ul>
+                    <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                                <path
+                                    d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                            </svg></a></li>
+                    <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                <path
+                                    d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                            </svg></a></li>
+                    <li><a href="#"><img src="img/pp.png" alt=""></a></li>
+                </ul>
+            </header>
+        </section>
+        <section class="list">
+            <div class="list-kolom">
+                <ul>
+                    <li><a href="#">Video</a></li>
+                    <li><a href="#">Gambar</a></li>
+                    <li><a href="#">Dan tidak begadang</a></li>
+                    <li><a href="#">Menurut Psikologi</a></li>
+                    <li><a href="#">Dalam 1 menit</a></li>
+                    <li><a href="#">Menurut Islam</a></li>
+                    <li><a href="#">Ibu Hamil</a></li>
+                    <li><a href="#">Bayi</a></li>
+                </ul>
+            </div>
+        </section>
+    </div>
+    <hr class="text-black">
+    <div class="container main">
+        <section>
+            <div class="ab">
+                <div class="bc">
+                    <h6>Sekitar 55.100.000 hasil (0,28 detik)</h6>
+                    <h3>Cara agar Cepat Tidur</h3>
+                    <ul>
+                        <li>Turunkan suhu udara</li>
+                        <li>Melakukan metode pernapasan 4-7-8. Untuk membantu tubuh rileks sebelum tidur</li>
+                        <li>Buat jadwal Tidur</li>
+                        <li>Matikan lampu</li>
+                        <li>Lakukan meditasi atau yoga</li>
+                        <li>Hindari tidur siang</li>
+                        <li>Mendengarkan musik</li>
+                        <li>Olahraga</li>
+                    </ul>
+                    <a href="#">item lainnya...</a> 17 Okt 2022
+                </div>
+                <div class="cd d-flex">
+                    <img src="img/foto.jpeg" alt="" class="img-fluid rounded-4">
+                    <img src="img/foto1.jpeg" alt="" class="img-fluid rounded-4">
+                </div>
+            </div>
+
+        </section>
+        <section class="isi-content col-md-8">
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/cnn.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>CNN Indonesia</p><br>
+                    <p>https://www.cnnindonesia.com/gaya-hidup</p>
+                </div>
+                <div class="link">
+                    1. <span><a href="#" class="udr">15 Cara agar cepat tidur agar terhindar dari
+                            insomnia</a></span>
+                </div>
+            </div>
+            <hr class="text-black" width="100%">
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/halodoc.jpg" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Halodoc</p><br>
+                    <p>https://www.halodoc.com/artikel/catat-ini-6-cara-ampuh-agar-bisa-cepat-tidur</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        2. <span><a href="#" class="udr">Catat, Ini 6 cara ampuh agar bisa cepat tidur
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">5 April 2022 - </span><b>Tidur</b> dan bangun diwaktu yang sama, Hindari
+                        interaksi dengan benda elektronik sebelum tidur, Tidak mengkonsumsi makanan dan minuman
+                        tertentu, mandi dengan air hangat, konsumsi makanan berkarbohidrat, berlatih pernapasan.<b> KLIK
+                            LINK DIATAS UNTUK KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/rupa-rupa.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Ruparupa</p><br>
+                    <p>https://www.ruparupa.com/blog/cara-cepat-tidur/</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        3. <span><a href="#" class="udr">7 Cara cepat tidur yang ampuh bagi penderita
+                                insomnia
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">4 April 2023 - </span><b>Pertama</b> Turunkan suhu ruangan, 2. Terapkan
+                        sistem pernapasan 4-7-8, 3. Atur Jadwal tidur, 4. Hindari melihat jam, 5. Gunakan aromaterapi,
+                        6. Kurangi konsumsi kafein, 7. Olahraga saat pagi atau sore hari<b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/alodok.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Alodokter</p><br>
+                    <p>https://www.alodokter.com/ini-cara-cepat-tidur-yang-mudah-dan-praktis</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        4. <span><a href="#" class="udr">6 Cara cepat tidur yang praktis dan efektif
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">5 Juni 2023 - </span><b>1.</b> Tidur di jam yang sama, 2. Matikan
+                        peralatan elektronik, 3. Hindari konsumsi makanan berlemak, 4. Mandi air hangat, 5. Konsumsi
+                        makanan berkarbohidrat,
+                        6. Lakukan latihan pernapasan<b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/kontan.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>KONTAN</p><br>
+                    <p>https://kesehatan.kontan.co.id/news/2-cara-cepat-tidur-nyenyak-hanya-butuh-waktu-2-menit</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        5. <span><a href="#" class="udr">2 Cara cepat tidur nyenyak, Hanya butuh waktu 2
+                                menit
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">23 Februari 2023 - </span><b>Cara</b> cepat tidur nyenyak • Konsisten
+                        terhadap waktu <b>tidur</b> dan bangun <b>tidur</b>, kurangi asupan kafein, nikotin, dan
+                        alkohol. Dan masih ada cara lain <b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/pyfa.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Pyfa Health</p><br>
+                    <p>https://pyfahealth.com/blog/7-cara-agar-bisa-cepat-tidur/</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        6. <span><a href="#" class="udr">7 Cara agar cepat tidur
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">10 Februari 2023 - </span>1. Buat jadwal tidur · 2. Matikan lampu · 3.
+                        Mendengarkan musik relaksasi · 4. Konsumsi makanan yang mengandung karbohidrat · 5. Hindari
+                        konsumsi makanan dan minuman tertentu, 6. menggurangi penggunaan elektronik, 7. Yoga<b> KLIK
+                            LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/kompas.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Kompas.com</p><br>
+                    <p>https://www.kompas.com/tren/read/2022/05/16/192900765/cara-cepat-tidur-nyenyak</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        7. <span><a href="#" class="udr">Cara cepat tidur nyenyak , Hanya butuh 10 - 60 detik
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">6 November 2022 - </span><b>Tips agar cepat tidur </b>dan nyenyak ·
+                        Konsisten terhadap waktu tidur dan bangun tidur. · Kurangi asupan kafein, nikotin, dan alkohol.
+                        · Hindari perangkat elektronik menjelang tidur · Pastikan kamar dalam keadaan gelap dan bebas
+                        dari gangguan<b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/detik.jpeg" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Detik.com</p><br>
+                    <p>https://www.detik.com/bali/berita/d-6538307/20-cara-tidur-cepat-dan-nyenyak
+                    </p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        8. <span><a href="#" class="udr">20 Cara cepat tidur dan nyenyak, cocok untuk orang
+                                insomnia
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">27 Januari 2023 - </span> Membuat pola tidur secara konsisten, Matikan
+                        lampu saat tidur, Hindari tidur siang, Hindari gadget, Membaca buku, Lakukan olahraga disiang
+                        hari, Hindari kopi, Lakukan Meditasi. Dan masih banyak lagi <b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/kompas.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Kompas.com</p><br>
+                    <p>https://www.kompas.com/sains/read/2021/8-cara-cepat-tidur-bagi-penderita-insomnia?page=all</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        9. <span><a href="#" class="udr">8 Cara cepat tidur bagi penderita insomnia Halaman
+                                all
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">8 November 2021 - </span>Cara <b>cepat tidur </b>bagi penderita insomnia ·
+                        1. Gunakan metode pernapasan 4-7-8 · 2. Mengatur jadwal tidur · 3. Berlatih yoga dan meditasi ·
+                        4. Hindari tidur siang · 5. Mendengarkan music · 6. Berolahraga secara rutin · 7. Hindari
+                        elektronik · 8. Batasi konsumsi kafein<b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="kolom">
+                <div class="main-content">
+                    <img src="img/gramed.png" class="img-thumbnail rounded-circle" alt="" width="50"
+                        height="50">
+                    <p>Gramedia</p><br>
+                    <p>https://www.gramedia.com/best-seller/cara-agar-cepat-tidur/</p>
+                </div>
+                <div class="mt-2">
+                    <div class="link">
+                        10. <span><a href="#" class="udr">9 Cara agar cepat tidur yang bisa kamu coba
+                                penderita insomnia
+                            </a></span>
+                    </div>
+                    <p><span class="abu-abu">13 September 2022 - </span>1. Tidur dan Bangun di Waktu yang Sama · 2.
+                        Hindari Interaksi dengan Benda Elektronik Sebelum Tidur · 3. Tidak Mengonsumsi Makanan atau
+                        Minuman<b> KLIK LINK DIATAS UNTUK
+                            KEJELASAN LEBIH LANJUT</b></p>
+                </div>
+            </div>
+            <div class="d-block mt-5">
+                <h2>Penelusuran terkait :</h2>
+                <div class="d-flex gap-5">
+                    <div class="terkait">
+                        <div class="pt"><a href="#" class="tkt"><b>cara </b>tidur cepat <b>30
+                                    detik</b></a></div>
+                        <div class="pt"><a href="#" class="tkt"><b>cara</b> tidur cepat <b>5
+                                    detik</b></a></div>
+                        <div class="pt"><a href="#" class="tkt"><b>cara </b>tidur cepat <b>10
+                                    detik</b></a></div>
+                        <div class="pt"><a href="#" class="tkt"><b>cara </b>agar <b>bisa </b>tidur
+                                <b>nyenyak</b></a></div>
+                        <div class="icn"></div>
+                    </div>
+                    <div class="terkait">
+                        <div class="pt"><a href="#" class="tkt"><b>cara </b>agar <b>bisa </b>tidur
+                                <b>siang</b></a></div>
+                        <div class="pt"><a href="#" class="tkt"><b>gerakan </b>agar cepat tidur</a>
+                        </div>
+                        <div class="pt"><a href="#" class="tkt"><b>minuman </b>agar cepat tidur</a>
+                        </div>
+                        <div class="pt"><a href="#" class="tkt"><b>doa </b>agar cepat tidur</a></div>
+                    </div>
+
+                </div>
+                <div class="page">
+                    <img src="img/page.png" alt="" class="img-thumbnail mx-auto d-block">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination  justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </section>
+    </div>
+    <footer>
+        <div class="ft">
+            <p>Indonesia <b>•Kec. Sawahan, Surabaya, Jawa timur - </b><a href="#">Berdasarkan aktifitas anda
+                    sebelumnya - Perbarui lokasi</a> </p>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
